a458d56d5ec36af47b625a3b6c8a5ff0eb551098
Hirasmous <61093942+Hirasmous@users.noreply.github.com>
2021-02-06 23:46:11 +0100

    Add files via upload

---
+++ lua/entities/npc_vj_l4d2_witch_s/shared.lua
@@ -0,0 +1,58 @@
+ENT.Base 			= "npc_vj_creature_base"
+ENT.Type 			= "ai"
+ENT.PrintName 		= "Witch"
+ENT.Author 			= "Hirasmous"
+ENT.Contact 		= "http://vrejgaming.webs.com/"
+ENT.Purpose 		= "Spawn it and fight with it!"
+ENT.Instructions 	= "Click on the spawnicon to spawn it."
+ENT.Category		= "Left 4 Dead 2"
+
+if (CLIENT) then
+local Name = "Witch"
+local LangName = "npc_vj_l4d2_witch_s"
+language.Add(LangName, Name)
+killicon.Add(LangName,"HUD/killicons/default",Color ( 255, 80, 0, 255 ) )
+language.Add("#"..LangName, Name)
+killicon.Add("#"..LangName,"HUD/killicons/default",Color ( 255, 80, 0, 255 ) )
+end
+
+if CLIENT then
+	net.Receive("L4D2WitchHUD",function(len,pl)
+		local isdeleted = net.ReadBool()
+		local entity = net.ReadEntity()
+
+		hook.Add("RenderScreenspaceEffects","L4D2WitchScreen",function()
+			local tab = {
+				["$pp_colour_addr"] = 0.3,
+				["$pp_colour_addg"] = 0.2,
+				["$pp_colour_addb"] = 0,
+				["$pp_colour_brightness"] = -0.2,
+				["$pp_colour_contrast"] = 0.6,
+				["$pp_colour_colour"] = 2,
+				["$pp_colour_mulr"] = 1,
+				["$pp_colour_mulg"] = 1,
+				["$pp_colour_mulb"] = 0.25
+			}
+			DrawColorModify(tab)
+		end)
+		if isdeleted == true then hook.Remove("RenderScreenspaceEffects","L4D2WitchScreen") end
+		hook.Add("PreDrawHalos","L4D2WitchHalo",function()
+			local tbL4D2Infected = {}
+			local tbL4D2Survivors = {}
+			for _,v in pairs(ents.GetAll()) do
+				if v:IsNPC() or v:IsPlayer() then
+					if string.find(v:GetClass(),"npc_vj_l4d*") then
+						table.insert(tbL4D2Infected,v)
+					else
+						if v:GetClass() != "obj_vj_bullseye" then
+							table.insert(tbL4D2Survivors,v)
+						end
+					end
+				end
+			end
+			halo.Add(tbL4D2Infected,Color(255,100,0),4,4,3,true,true)
+			halo.Add(tbL4D2Survivors,Color(0,0,255),4,4,3,true,true)
+		end)
+		if isdeleted == true then hook.Remove("PreDrawHalos","L4D2WitchHalo") end
+	end)
+end
