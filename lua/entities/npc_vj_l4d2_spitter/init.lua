04b2c62d158294ac0d18959f239d36745f0a1059
Hirasmous <61093942+Hirasmous@users.noreply.github.com>
2021-02-09 23:57:53 +0100

    Removed "Shoved_Forward"

--- lua/entities/npc_vj_l4d2_spitter/init.lua
+++ lua/entities/npc_vj_l4d2_spitter/init.lua
@@ -183,7 +183,7 @@ function ENT:SetGhost(bool)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnTakeDamage_AfterDamage(dmginfo,hitgroup)
-    local anims = VJ_PICK{"Shoved_BackWard","Shoved_Leftward","Shoved_Forward","Shoved_Rightward"}
+    local anims = VJ_PICK{"Shoved_BackWard","Shoved_Leftward","Shoved_Rightward"}
     if dmginfo:GetDamageType() == DMG_CLUB || dmginfo:GetDamageType() == DMG_GENERIC then
         self:VJ_ACT_PLAYACTIVITY(anims,true,VJ_GetSequenceDuration(self,anims),false)
     end
