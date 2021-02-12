ef6fa6b14c380ff94272b17f7587f92f781646c0
Hirasmous <61093942+Hirasmous@users.noreply.github.com>
2021-02-12 18:15:56 +0100

    Added convar for damage

--- lua/entities/obj_vj_l4d2_acidpuddle/init.lua
+++ lua/entities/obj_vj_l4d2_acidpuddle/init.lua
@@ -34,15 +34,17 @@ function ENT:CustomOnThink()
 	local sphere = ents.FindInSphere(self:GetPos(),50)
 	local owner = self:GetOwner()
 	for k, v in ipairs(sphere) do
-		if IsValid(v) then 
-			if v:IsNPC() || v:IsNextBot() || (v:IsPlayer() && v:Alive() && GetConVar("ai_ignoreplayers"):GetInt() == 0) then
-				if v:IsNPC() && (v:Classify() == CLASS_ZOMBIE || v.IsVJBaseSNPC && table.HasValue(v.VJ_NPC_Class, "CLASS_ZOMBIE")) then return end
-	    	    if math.random(1,5) == 5 then
-	    		    VJ_EmitSound(self,VJ_PICKRANDOMTABLE({"player/pz/hit/zombie_slice_1.wav","player/pz/hit/zombie_slice_2.wav","player/pz/hit/zombie_slice_3.wav","player/pz/hit/zombie_slice_4.wav","player/pz/hit/zombie_slice_5.wav","player/pz/hit/zombie_slice_6.wav"}),65,math.random(100,100))
-		            v:TakeDamage(math.random(5, 10), self:GetOwner() or self, self:GetOwner() or self)
-	    		end
-	        end
-	    end
+		if GetConVarNumber("vj_l4d2_goodamage") == 1 then
+			if IsValid(v) then 
+				if v:IsNPC() || v:IsNextBot() || (v:IsPlayer() && v:Alive() && GetConVar("ai_ignoreplayers"):GetInt() == 0) then
+					if v:IsNPC() && (v:Classify() == CLASS_ZOMBIE || v.IsVJBaseSNPC && table.HasValue(v.VJ_NPC_Class, "CLASS_ZOMBIE")) then return end
+		    	    if math.random(1,5) == 5 then
+		    		    VJ_EmitSound(self,VJ_PICKRANDOMTABLE({"player/pz/hit/zombie_slice_1.wav","player/pz/hit/zombie_slice_2.wav","player/pz/hit/zombie_slice_3.wav","player/pz/hit/zombie_slice_4.wav","player/pz/hit/zombie_slice_5.wav","player/pz/hit/zombie_slice_6.wav"}),65,math.random(100,100))
+			            v:TakeDamage(math.random(5, 10), self:GetOwner() or self, self:GetOwner() or self)
+		    		end
+		        end
+		    end
+		end
     end
 end
 /*-----------------------------------------------
