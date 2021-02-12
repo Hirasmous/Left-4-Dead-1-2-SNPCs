a107866b242c5c7d2e0ed6c58e1d2f4336d7d405
Hirasmous <61093942+Hirasmous@users.noreply.github.com>
2021-02-12 23:02:29 +0100

    Adjusted 3rd person camera

--- lua/entities/npc_vj_l4d2_witch_w/init.lua
+++ lua/entities/npc_vj_l4d2_witch_w/init.lua
@@ -38,7 +38,7 @@ ENT.HasDeathAnimation = true
 ENT.AnimTbl_Death = {ACT_DIE_GUTSHOT}
 ENT.VJC_Data = {
 	CameraMode = 1, -- Sets the default camera mode | 1 = Third Person, 2 = First Person
-	ThirdP_Offset = Vector(0, 0, 0), -- The offset for the controller when the camera is in third person
+	ThirdP_Offset = Vector(40, 10, -50), -- The offset for the controller when the camera is in third person
 	FirstP_Bone = "ValveBiped.Bip01_Head1", -- If left empty, the base will attempt to calculate a position for first person
 	FirstP_Offset = Vector(3, 0, 5), -- The offset for the controller when the camera is in first person
 }
