9c6d452f3e7eb7cc3026597d3cb534b4fa30ba68
Tensor2 <78664773+Tensor2@users.noreply.github.com>
2021-02-19 15:09:17 -0600

    Update prop_anim_survivor.lua

--- lua/entities/prop_anim_survivor.lua
+++ lua/entities/prop_anim_survivor.lua
@@ -8,7 +8,7 @@ ENT.RenderGroup = RENDERGROUP_BOTH
 ENT.AutomaticFrameAdvance = true
 
 --Server
-ENT.Model = "models/survivors/L4D2_Human_base.mdl"
+--ENT.Model = "models/survivors/L4D2_Human_base.mdl"
 ENT.Owner = nil
 
 ENT.moveX = 0
@@ -23,22 +23,23 @@ ENT.bIsBeingPounded = false
 ENT.bIsBeingShredded = false
 
 if SERVER then
-    function ENT:Initialize()
-        self:SetRenderMode(1)
-        self:SetColor(Color(0, 0, 0, 0))
-        self:DrawShadow(false)
-        self.Owner = self:GetOwner()
-    end
+	function ENT:Initialize()
+		self.Owner = self:GetOwner()
+	end
 
-    function ENT:Think()
-        if self.bIsBeingPounced then
-            self:SetPoseParameter("move_x", math.Round(self.moveX))
-            self:SetPoseParameter("move_y", math.Round(self.moveY))
-            self:SetPoseParameter("jockey_x", math.Round(self.moveX))
-            self:SetPoseParameter("jockey_y", math.Round(self.moveY))
-        end
+	function ENT:Think()
+		if self.bIsBeingPounced then
+			self:SetPoseParameter("move_x", math.Round(self.moveX))
+			self:SetPoseParameter("move_y", math.Round(self.moveY))
+			self:SetPoseParameter("jockey_x", math.Round(self.moveX))
+			self:SetPoseParameter("jockey_y", math.Round(self.moveY))
+		end
 
-        self:NextThink(CurTime())
-        return true
-    end
+		if self:GetParent():GetClass() == self:GetClass() then
+			self:SetCycle(self:GetParent():GetCycle())
+		end
+
+		self:NextThink(CurTime())
+		return true
+	end
 end
