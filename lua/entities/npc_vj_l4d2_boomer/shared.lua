c4c1ffdb5932e730b36c47aa6b5576e9562e0935
Tensor2 <78664773+Tensor2@users.noreply.github.com>
2021-02-25 01:09:42 -0600

    Improved vomit overlay & added vomit cover

--- lua/entities/npc_vj_l4d2_boomer/shared.lua
+++ lua/entities/npc_vj_l4d2_boomer/shared.lua
@@ -8,26 +8,16 @@ ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Left 4 Dead 2"
 
 if (CLIENT) then
-        local Name = "Boomer"
-        local LangName = "npc_vj_l4d2_boomer"
-        language.Add(LangName, Name)
-        killicon.Add(LangName,"HUD/killicons/default",Color ( 255, 80, 0, 255 ) )
-        language.Add("#"..LangName, Name)
-        killicon.Add("#"..LangName,"HUD/killicons/default",Color ( 255, 80, 0, 255 ) )
+		local Name = "Boomer"
+		local LangName = "npc_vj_l4d2_boomer"
+		language.Add(LangName, Name)
+		killicon.Add(LangName,"HUD/killicons/default",Color ( 255, 80, 0, 255 ) )
+		language.Add("#"..LangName, Name)
+		killicon.Add("#"..LangName,"HUD/killicons/default",Color ( 255, 80, 0, 255 ) )
 end
 
 if CLIENT then
-    hook.Add("HUDPaint","hkHandleBoomerBlastRed", function()
-		if LocalPlayer():Alive() && LocalPlayer().m_bRenderBoomerScreenFX then
-			--local hMaterial = Material("models/props_lab/Tank_Glass001")
-			surface.SetMaterial(Material("models/props_lab/Tank_Glass001"))
-			surface.SetDrawColor(255,255,255,LocalPlayer().m_iBoomerScreenFXAlpha)
-			surface.DrawTexturedRect(0,0,ScrW(),ScrH())
-			DrawMotionBlur( 0.5, (LocalPlayer().m_iBoomerScreenFXAlpha / 255)+1, 0.01 )
-		end
-		local color_red = Color( 255, 0, 0 )
-	end)
-    net.Receive("L4D2BoomerHUD",function(len,pl)
+	net.Receive("L4D2BoomerHUD",function(len,pl)
 		local isdeleted = net.ReadBool()
 		local entity = net.ReadEntity()
 
@@ -47,42 +37,42 @@ if CLIENT then
 		end)
 		if isdeleted == true then hook.Remove("RenderScreenspaceEffects","L4D2BoomerScreen") end
 		local chargemat = surface.GetTextureID( "vgui/hud/pz_charge_boomer" )
-        local chargemat2 = surface.GetTextureID( "vgui/hud/PZ_charge_meter" )
-        local chargemat3 = surface.GetTextureID( "vgui/hud/PZ_charge_bg" )
-        local chargemat4 = surface.GetTextureID( "vgui/hud/PZ_charge_boomer_fill" )
-        local nextattack = IsValid(entity) && entity:GetNW2Int("VomitT") -CurTime() or 0
+		local chargemat2 = surface.GetTextureID( "vgui/hud/PZ_charge_meter" )
+		local chargemat3 = surface.GetTextureID( "vgui/hud/PZ_charge_bg" )
+		local chargemat4 = surface.GetTextureID( "vgui/hud/PZ_charge_boomer_fill" )
+		local nextattack = IsValid(entity) && entity:GetNW2Int("VomitT") -CurTime() or 0
 		if IsValid(entity) && entity:GetNW2Int("VomitT") < CurTime() then nextattack = 1 end
 
-        hook.Add( "HUDPaint", "BoomerChargeHUD", function()
-        	surface.SetDrawColor( 255, 255, 255, 255 ) 
-	        surface.SetTexture(chargemat3) 
-	        surface.DrawTexturedRect( ScrW() / 1.173, ScrH() / 1.37, ScrW() / 7.8, ScrH() / 4 ) 
-	        
-	        surface.SetTexture(chargemat) 
-	        surface.DrawTexturedRect( ScrW() / 1.15, ScrH() / 1.28, ScrW() / 10.4, ScrH() / 6 )
-	        if nextattack != 1 then
+		hook.Add( "HUDPaint", "BoomerChargeHUD", function()
+			surface.SetDrawColor( 255, 255, 255, 255 ) 
+			surface.SetTexture(chargemat3) 
+			surface.DrawTexturedRect( ScrW() / 1.173, ScrH() / 1.37, ScrW() / 7.8, ScrH() / 4 ) 
+			
+			surface.SetTexture(chargemat) 
+			surface.DrawTexturedRect( ScrW() / 1.15, ScrH() / 1.28, ScrW() / 10.4, ScrH() / 6 )
+			if nextattack != 1 then
 				surface.SetDrawColor(255,255,255,255 /nextattack)
 			else
 				surface.SetDrawColor(255,255,255,math.abs(math.sin(CurTime() *3) *255))
 			end
 
 			surface.SetTexture(chargemat4) 
-	        surface.DrawTexturedRect( ScrW() / 1.2, ScrH() / 1.1, ScrW() / 22.4, ScrH() / 12.5 ) 
-	        if nextattack != 1 then
+			surface.DrawTexturedRect( ScrW() / 1.2, ScrH() / 1.1, ScrW() / 22.4, ScrH() / 12.5 ) 
+			if nextattack != 1 then
 				surface.SetDrawColor(255,255,255,255 /nextattack)
 			else
 				surface.SetDrawColor(255,255,255,math.abs(math.sin(CurTime() *3) *255))
 			end
 
-	        surface.SetTexture(chargemat2) 
-	        surface.DrawTexturedRect( ScrW() / 1.15, ScrH() / 1.28, ScrW() / 10.4, ScrH() / 6 )  
-	        if nextattack != 1 then
+			surface.SetTexture(chargemat2) 
+			surface.DrawTexturedRect( ScrW() / 1.15, ScrH() / 1.28, ScrW() / 10.4, ScrH() / 6 )  
+			if nextattack != 1 then
 				surface.SetDrawColor(255,255,255,255 /nextattack)
 			else
 				surface.SetDrawColor(255,255,255,math.abs(math.sin(CurTime() *3) *255))
 			end
-        end)
-        if isdeleted == true then hook.Remove("HUDPaint","BoomerChargeHUD") end
+		end)
+		if isdeleted == true then hook.Remove("HUDPaint","BoomerChargeHUD") end
 		hook.Add("PreDrawHalos","L4D2BoomerHalo",function()
 			local tbL4D2Infected = {}
 			local tbL4D2GhostedInfected = {}
@@ -91,7 +81,7 @@ if CLIENT then
 				if v:IsNPC() or v:IsPlayer() then
 					if string.find(v:GetClass(),"npc_vj_l4d*") then
 						if v:GetNW2Bool("Ghosted") then
-						    table.insert(tbL4D2GhostedInfected,v)
+							table.insert(tbL4D2GhostedInfected,v)
 						else
 							table.insert(tbL4D2Infected,v)
 						end
@@ -128,12 +118,12 @@ if CLIENT then
 		end)
 		if isdeleted == true then hook.Remove("RenderScreenspaceEffects","GhostL4D2BoomerScreen") end
 		local chargemat = Material( "vgui/hud/pz_charge_boomer" )
-        local chargemat2 = Material( "vgui/hud/PZ_charge_meter" )
-        local chargemat3 = surface.GetTextureID( "vgui/hud/PZ_charge_bg" )
-        local chargemat4 = surface.GetTextureID( "vgui/hud/PZ_charge_boomer_fill" )
+		local chargemat2 = Material( "vgui/hud/PZ_charge_meter" )
+		local chargemat3 = surface.GetTextureID( "vgui/hud/PZ_charge_bg" )
+		local chargemat4 = surface.GetTextureID( "vgui/hud/PZ_charge_boomer_fill" )
 
-        hook.Add( "HUDPaint", "GhostBoomerChargeHUD", function()
-        	surface.SetDrawColor( 255, 255, 255, 255 ) 
+		hook.Add( "HUDPaint", "GhostBoomerChargeHUD", function()
+			surface.SetDrawColor( 255, 255, 255, 255 ) 
 			surface.SetTexture(chargemat3) 
 			surface.DrawTexturedRect( ScrW() / 1.173, ScrH() / 1.37, ScrW() / 7.8, ScrH() / 4 )
 
@@ -145,11 +135,11 @@ if CLIENT then
 			surface.SetMaterial(chargemat2) 
 			surface.DrawTexturedRect( ScrW() / 1.15, ScrH() / 1.28, ScrW() / 10.4, ScrH() / 6 ) 
 
-	        surface.SetTexture(chargemat4) 
-	        surface.DrawTexturedRect( ScrW() / 1.2, ScrH() / 1.1, ScrW() / 22.4, ScrH() / 12.5 )
-     
-        end)
-        if isdeleted == true then hook.Remove("HUDPaint","GhostBoomerChargeHUD") end
+			surface.SetTexture(chargemat4) 
+			surface.DrawTexturedRect( ScrW() / 1.2, ScrH() / 1.1, ScrW() / 22.4, ScrH() / 12.5 )
+	
+		end)
+		if isdeleted == true then hook.Remove("HUDPaint","GhostBoomerChargeHUD") end
 		hook.Add("PreDrawHalos","GhostL4D2BoomerHalo",function()
 			local tbL4D2Infected = {}
 			local tbL4D2GhostedInfected = {}
@@ -158,7 +148,7 @@ if CLIENT then
 				if v:IsNPC() or v:IsPlayer() then
 					if string.find(v:GetClass(),"npc_vj_l4d*") then
 						if v:GetNW2Bool("Ghosted") then
-						    table.insert(tbL4D2GhostedInfected,v)
+							table.insert(tbL4D2GhostedInfected,v)
 						else
 							table.insert(tbL4D2Infected,v)
 						end
@@ -176,16 +166,63 @@ if CLIENT then
 		if isdeleted == true then hook.Remove("PreDrawHalos","GhostL4D2BoomerHalo") end
 	end)
 
-
-	net.Receive("nDoBoomerBlast",function()
-		LocalPlayer().m_bRenderBoomerScreenFX = true
-		LocalPlayer().m_iBoomerScreenFXAlpha = 255
-
-		timer.Create("tmKillScreenFX_Boomer",0.3,255,function()
-			LocalPlayer().m_iBoomerScreenFXAlpha = LocalPlayer().m_iBoomerScreenFXAlpha - 8
-			if LocalPlayer().m_iBoomerScreenFXAlpha == 1 then
-				LocalPlayer().m_bRenderBoomerScreenFX = false
+	net.Receive("nBoomer_InitializeOverlay", function()
+		hook.Add("RenderScreenspaceEffects", "hkBoomer_VomitOverlay", function()
+			DrawMaterialOverlay("models/props_lab/Tank_Glass001", 1)
+		end)
+		timer.Create("tmBoomer_RemoveVomitOverlay", 0.1, 150, function()
+			local tm = "tmBoomer_RemoveVomitOverlay"
+			if not LocalPlayer():Alive() then
+				hook.Remove("RenderScreenspaceEffects", "hkBoomer_VomitOverlay")
+				timer.Stop(tm)
+			else
+				if timer.RepsLeft(tm) <= 100 && timer.RepsLeft(tm) > 1 then
+					local fr = 0.01 * timer.RepsLeft(tm)
+					hook.Add("RenderScreenspaceEffects", "hkBoomer_VomitOverlay", function()
+						DrawMaterialOverlay("models/props_lab/Tank_Glass001", fr)
+					end)
+				end
+				if timer.RepsLeft(tm) <= 1 then
+					hook.Remove("RenderScreenspaceEffects", "hkBoomer_VomitOverlay")
+				end
 			end
+		end)
+	end)
+
+	net.Receive("nBoomer_InitializeResidue", function()
+		local bDead = net.ReadBool()
+		local pos = net.ReadVector()
+		local id  = net.ReadString()
+		local _ents 
+		if bDead == true then
+			_ents = ents.FindInSphere(pos, 135)
+		else
+			_ents = ents.FindInCone(pos, ents.GetByIndex(tonumber(id)):GetForward() * 275, 250, math.cos(20))
+		end
+		function VomitCover()
+			local mat = Material("models/vj_l4d2/survivors/survivor_it", "alphatest")
+			cam.Start3D(EyePos(), EyeAngles())
+				for k, v in ipairs(_ents) do
+					if (IsValid(v) && util.IsValidModel(v:GetModel() or "")) && (v:IsNPC() || v:IsNextBot() || (v:IsPlayer() && v:Alive())) then
+						render.SetBlend(0.8)
+						render.MaterialOverride(mat)
+						v:DrawModel()
+						render.SetBlend(1)
+						render.MaterialOverride(0)
+					end
+				end
+			cam.End3D()
+		end
+		hook.Add("RenderScreenspaceEffects", "hkBoomer"..id.."_VomitResidue", VomitCover)
+		timer.Create("tmBoomer"..id.."_RemoveVomitResidue", 0.1, 150, function()
+			local tm = "tmBoomer"..id.."_RemoveVomitResidue"
+			if timer.RepsLeft(tm) <= 1 then
+				hook.Remove("RenderScreenspaceEffects", "hkBoomer"..id.."_VomitResidue")
+			end
+		end)
+		hook.Add("PreCleanupMap", "hkBoomer_CleanupMap", function()
+			hook.Remove("RenderScreenspaceEffects", "hkBoomer"..id.."_VomitResidue")
+			timer.Stop("tmBoomer"..id.."_RemoveVomitResidue")
 		end)
 	end)
 end
