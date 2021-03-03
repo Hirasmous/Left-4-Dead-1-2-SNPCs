99d1091c57755859f9c7f5ad7c1eef2698056abc
Tensor2 <78664773+Tensor2@users.noreply.github.com>
2021-03-03 17:02:37 -0600

    Update init.lua

--- lua/entities/npc_vj_l4d2_hunter/init.lua
+++ lua/entities/npc_vj_l4d2_hunter/init.lua
@@ -321,6 +321,8 @@ function ENT:DismountHunter()
 		if table.Count(self.tblEnemyWeapons) > 0 then
 			for i = 1, table.Count(self.tblEnemyWeapons) do
 				local tbl = self.tblEnemyWeapons
+				enemy.VJ_CanBePickedUpWithOutUse = true
+				enemy.VJ_CanBePickedUpWithOutUse_Class = tbl[i][1]
 				enemy:Give(tbl[i][1], true)
 				local wpn = enemy:GetWeapon(tbl[i][1])
 				if tbl[i][2][1] ~= -1 then
@@ -329,6 +331,9 @@ function ENT:DismountHunter()
 				if tbl[i][3][1] ~= -1 then
 					wpn:SetClip2(tbl[i][3][2])
 				end
+			end
+			if enemy:HasWeapon(self.EnemyActiveWeapon) then
+				enemy:SetActiveWeapon(enemy:GetWeapon(self.EnemyActiveWeapon))
 			end
 		end
 		for a, c in ipairs(self.tblEnemyAmmo) do
