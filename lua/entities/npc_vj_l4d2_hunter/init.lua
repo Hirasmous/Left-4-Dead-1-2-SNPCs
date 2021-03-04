e9327f87239228a53cf2790c124201ecf0aecf3c
Tensor2 <78664773+Tensor2@users.noreply.github.com>
2021-03-03 22:21:35 -0600

    Update init.lua

--- lua/entities/npc_vj_l4d2_hunter/init.lua
+++ lua/entities/npc_vj_l4d2_hunter/init.lua
@@ -840,14 +840,7 @@ function ENT:CustomOnPriorToKilled(dmginfo,hitgroup)
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnDeath_AfterCorpseSpawned(dmginfo, hitgroup, GetCorpse)
-	local attacker = dmginfo:GetAttacker()
-	if IsValid(attacker) then
-		if attacker:IsNPC() then
-			PrintMessage(HUD_PRINTTALK, attacker:GetName().." killed ".. self:GetName())
-		elseif attacker:IsPlayer() then
-			PrintMessage(HUD_PRINTTALK, attacker:Nick().." killed ".. self:GetName())
-		end
-	end
+	self:L4D2_DeathMessage(dmginfo:GetAttacker())
 	self:DismountHunter()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
