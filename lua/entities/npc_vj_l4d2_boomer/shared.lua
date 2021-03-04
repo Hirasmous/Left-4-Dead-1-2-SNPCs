a59130980cd569ad4f09ca635502b1e5692e2d19
Tensor2 <78664773+Tensor2@users.noreply.github.com>
2021-03-04 12:24:23 -0600

    Vomit residue improvements

--- lua/entities/npc_vj_l4d2_boomer/shared.lua
+++ lua/entities/npc_vj_l4d2_boomer/shared.lua
@@ -211,7 +211,7 @@ if CLIENT then
 			local mat = Material("models/vj_l4d2/survivors/survivor_it", "alphatest")
 			cam.Start3D(EyePos(), EyeAngles())
 				for k, v in ipairs(_ents) do
-					if (IsValid(v) && util.IsValidModel(v:GetModel() or "")) && (v:IsNPC() || v:IsNextBot() || (v:IsPlayer() && v:Alive())) then
+					if (IsValid(v) && util.IsValidModel(v:GetModel() or "")) && v:GetNoDraw() == false && ( (v:GetClass() == "prop_dynamic" && v:GetParent():GetClass() == "prop_anim_survivor") || v:IsNPC() && not string.find(v:GetClass(), "_l4d") || v:IsNextBot() || (v:IsPlayer() && v:Alive()) ) then
 						render.SetBlend(0.8)
 						render.MaterialOverride(mat)
 						v:DrawModel()
