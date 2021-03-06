81b37cd55becdf4a8d4c4e2e99284d8577859806
Tensor2 <78664773+Tensor2@users.noreply.github.com>
2021-03-05 18:35:06 -0600

    Modified incap damage

--- lua/entities/npc_vj_l4d2_smoker/init.lua
+++ lua/entities/npc_vj_l4d2_smoker/init.lua
@@ -181,7 +181,7 @@ function ENT:CustomOnAcceptInput(key,activator,caller,data)
 		if self.IsEnemyStuck == true || self:GetSequence() == self:LookupSequence(self.IncapAnimation) then
 			local incapent = self.pIncapacitatedEnemy
 			if IsValid(incapent) then
-				incapent:TakeDamage(5, self, incapent)
+				incapent:TakeDamage(math.random(2, 4), self, incapent)
 				VJ_CreateSound(incapent,VJ_PICKRANDOMTABLE{"player/pz/hit/zombie_slice_1.mp3","player/pz/hit/zombie_slice_2.mp3","player/pz/hit/zombie_slice_3.mp3","player/pz/hit/zombie_slice_4.mp3","player/pz/hit/zombie_slice_5.mp3","player/pz/hit/zombie_slice_6.mp3"},65,self:VJ_DecideSoundPitch(100,100))
 			end
 		end
